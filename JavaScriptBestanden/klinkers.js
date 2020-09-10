74c8fc3aa21d0ab34995094c2a456f652a0ddeca
MarienL1995 <lander.marien@vdabcampus.be>
2020-09-10 16:25:54 +0200

    Ging beter als eerste oefening

---
+++ JavaScriptBestanden/klinkers.js
@@ -0,0 +1,7 @@
+let teller = 0
+function telKlinkers () {
+    switch(document.getElementById('tekstVak').onkeyup)
+    
+        
+}
+//Ging de goede richting op. extra uitleg verkregen van coach.//
